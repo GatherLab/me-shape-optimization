[memory] Fix memory leakage by initializing gmsh and pyvista before plotting

example.py
@@ -19,6 +19,10 @@ import pyvista
 
 pyvista.start_xvfb()
 
+import gmsh
+
+gmsh.initialize()
+
 L, H, B = 12e-3, 0.2e-3, 3e-3
 
 Bmin = 1e-3
@@ -26,12 +30,36 @@ Bmax = B
 grid_size = 0.5e-3
 
 # Select boundary conditions to apply
-bc_z = False
-bc_y = False
-no_eigenvalues = 25
+bc_z = True
+bc_y = True
 
-# Init eigensolver
+no_eigenvalues = 20
+target_frequency = 130e3
+
+## Define eigensolver
 eigensolver = SLEPc.EPS().create(MPI.COMM_WORLD)
+
+# Set problem
+eigensolver.setProblemType(SLEPc.EPS.ProblemType.GHEP)
+# Shift and invert mode
+st = eigensolver.getST()
+st.setType(SLEPc.ST.Type.SINVERT)
+# target real eigenvalues
+eigensolver.setWhichEigenpairs(SLEPc.EPS.Which.TARGET_REAL)
+# Set the target frequency
+eigensolver.setTarget(target_frequency**2 * 2 * np.pi)
+# Set no of eigenvalues to compute
+eigensolver.setDimensions(nev=no_eigenvalues)
+
+## Plotting
+# Now define pyvista plotter
+plotter = pyvista.Plotter(off_screen=True)
+
+## Geometry generation
+gmsh.option.setNumber("General.Terminal", 0)
+model = gmsh.model()
+model.add("Box")
+model.setCurrent("Box")
 
 # Generate random numbers using numpy library with Bmax as a maximum and Bmin as
 # a minimum with L/grid_size entries
@@ -50,16 +78,17 @@ geometry_width_list = [
     0.003000,
     0.003000,
     0.003000,
+    0.003000,
+    0.003000,
+    0.003000,
+    0.003000,
     0.001000,
+    0.003000,
     0.001000,
+    0.003000,
     0.001000,
-    0.001000,
-    0.001000,
-    0.001000,
-    0.001000,
-    0.001000,
-    0.001000,
-    0.001000,
+    0.003000,
+    0.003000,
 ]
 # geometry_width_list = np.repeat(B, int(L / grid_size))
 
@@ -86,18 +115,16 @@ V, eigenvalues, eigenmodes, first_longitudinal_mode = unified_solving_function(
 # For only constraining the oscillation in the y-direction, the relevant first longitudinal mode is around the 4th mode
 # For constraining the oscillation in the y & z-direction, the relevant first longitudinal mode is around the 19th mode
 
-"""
 # Plot all eigenmodes
 for mode_no in range(np.size(eigenvalues)):
     saving_path = "deflection{i}.png".format(i=mode_no)
     visualise_3D(V, eigenvalues, eigenmodes, mode_no, saving_path)
 
-determine_first_longitudinal_mode(V, eigenmodes)
-"""
+determine_first_longitudinal_mode(V, eigenmodes, eigenvalues, target_frequency)
 
 # Plot first longitudinal mode only
-freq_3D = np.sqrt(eigenvalues[first_longitudinal_mode].real) / 2 / np.pi
-saving_path = "{i:.2f}.png".format(i=freq_3D)
-visualise_3D(
-    V, eigenvalues, eigenmodes, first_longitudinal_mode, saving_path, viewup=True
-)
+# freq_3D = np.sqrt(eigenvalues[first_longitudinal_mode].real) / 2 / np.pi
+# saving_path = "{i:.2f}.png".format(i=freq_3D)
+# visualise_3D(
+# V, eigenvalues, eigenmodes, first_longitudinal_mode, saving_path, viewup=True
+# )

examples_and_proof_of_concepts/geometry_generator.py
@@ -21,19 +21,14 @@ def generate_geometry(L, H, B):
     return geometry_mesh
 
 
-def generate_gmsh_mesh(L, H, B, geometry_width_list):
+def generate_gmsh_mesh(model, L, H, B, geometry_width_list):
     """
     https://jsdokken.com/src/tutorial_gmsh.html
     """
+    gmsh.clear()
     N = int(np.size(geometry_width_list))
 
-    gmsh.initialize()
-
-    # Choose if Gmsh output is verbose
-    gmsh.option.setNumber("General.Terminal", 0)
-    model = gmsh.model()
-    model.add("Box")
-    model.setCurrent("Box")
+    # gmsh.initialize()
 
     # The boxes are defined by a point x,y,z and the length in the x,y,z direction
     # Define a list of boxes with a unit lenght of L/N, height H and width B

examples_and_proof_of_concepts/memory_leakage_investigation.py
@@ -1,6 +1,10 @@
 import sys
 import psutil
 import os
+import gc
+import tracemalloc
+
+tracemalloc.start()
 
 import numpy as np
 import matplotlib.pylab as plt
@@ -22,6 +26,10 @@ import pyvista
 import pandas as pd
 
 pyvista.start_xvfb()
+
+import gmsh
+
+gmsh.initialize()
 
 print(
     "memory start: {0}".format(
@@ -376,32 +384,18 @@ def unified_solving_function(eigensolver, geometry_mesh):
     # changes quite a bit though.
 
     ## Takes about 13 MB RAM
-    K = fem.petsc.assemble_matrix(k, bcs=[bc1])
-    M = fem.petsc.assemble_matrix(m, bcs=[bc1])
+    K = fem.petsc.assemble_matrix(k, bcs=[])
+    M = fem.petsc.assemble_matrix(m, bcs=[])
 
     K.assemble()
     M.assemble()
 
     # Define eigensolver and solve for eigenvalues
-    no_eigenvalues = 30
+    no_eigenvalues = 20
     target_frequency = 100000
 
     eigensolver.setOperators(K, M)
 
-    eigensolver.setProblemType(SLEPc.EPS.ProblemType.GHEP)
-    # tol = 1e-9
-    # eigensolver.setTolerances(tol=tol)
-
-    # Shift and invert mode
-    st = eigensolver.getST()
-    st.setType(SLEPc.ST.Type.SINVERT)
-    # target real eigenvalues
-    eigensolver.setWhichEigenpairs(SLEPc.EPS.Which.TARGET_REAL)
-    # Set the target frequency
-    eigensolver.setTarget(target_frequency**2 * 2 * np.pi)
-    # Set no of eigenvalues to compute
-    eigensolver.setDimensions(nev=no_eigenvalues)
-    ##
     eigensolver.solve()
     evs = eigensolver.getConverged()
 
@@ -428,8 +422,22 @@ def unified_solving_function(eigensolver, geometry_mesh):
     print(psutil.Process(os.getpid()).memory_info().rss / 1024**2)
 
 
+target_frequency = 100e3
+no_eigenvalues = 20
 # Eigensolver has to be defined once and then updated to prevent RAM leakage
 eigensolver = SLEPc.EPS().create(MPI.COMM_WORLD)
+
+# Shift and invert mode
+st = eigensolver.getST()
+st.setType(SLEPc.ST.Type.SINVERT)
+# target real eigenvalues
+eigensolver.setWhichEigenpairs(SLEPc.EPS.Which.TARGET_REAL)
+# Set the target frequency
+eigensolver.setTarget(target_frequency**2 * 2 * np.pi)
+# Set no of eigenvalues to compute
+eigensolver.setDimensions(nev=no_eigenvalues)
+eigensolver.setProblemType(SLEPc.EPS.ProblemType.GHEP)
+##
 
 L, H, B = 12e-3, 0.2e-3, 3e-3
 # Geometry initialization
@@ -440,6 +448,12 @@ geometry_mesh = mesh.create_box(
     cell_type=mesh.CellType.tetrahedron,
 )
 
+# Choose if Gmsh output is verbose
+gmsh.option.setNumber("General.Terminal", 0)
+model = gmsh.model()
+model.add("Box")
+model.setCurrent("Box")
+
 i = 1
 while True:
     print(i)
@@ -449,17 +463,23 @@ while True:
     grid_size = 0.5e-3
     # else:
     # L, H, B = 11e-3, 0.2e-3, 3e-3
+    print(psutil.Process(os.getpid()).memory_info().rss / 1024**2)
     geometry_width_list = np.random.uniform(1e-3, B, int(L / grid_size))
-    gmsh_mesh = generate_gmsh_mesh(L, H, B, geometry_width_list)
+    print(psutil.Process(os.getpid()).memory_info().rss / 1024**2)
+    gmsh_mesh = generate_gmsh_mesh(model, L, H, B, geometry_width_list)
 
+    print(psutil.Process(os.getpid()).memory_info().rss / 1024**2)
     geometry_mesh = mesh.create_box(
         MPI.COMM_WORLD,
         [np.array([0, 0, 0]), np.array([L, H, B])],
         [20, 2, 5],
         cell_type=mesh.CellType.tetrahedron,
     )
+    print(psutil.Process(os.getpid()).memory_info().rss / 1024**2)
     ##
     unified_solving_function(eigensolver, geometry_mesh)
+    print(psutil.Process(os.getpid()).memory_info().rss / 1024**2)
+
     i += 1
 
 

scipy_functions.py
@@ -1,12 +1,10 @@
-from geometry_generator import generate_geometry, generate_gmsh_mesh
-from solver import unified_solving_function, determine_first_longitudinal_mode
+from geometry_generator import generate_gmsh_mesh
+from solver import unified_solving_function
 from visualisation import visualise_3D, append_feature
-
-from dolfinx.io.gmshio import model_to_mesh
 
 import sys
 import os
-import numpy as np
+import psutil
 
 from mpi4py import MPI
 from slepc4py import SLEPc
@@ -15,8 +13,26 @@ import slepc4py
 slepc4py.init(sys.argv)
 
 import pyvista
+import numpy as np
 
 pyvista.start_xvfb()
+
+from scipy.optimize import (
+    shgo,
+    differential_evolution,
+    dual_annealing,
+    direct,
+    minimize,
+)
+
+import gmsh
+
+gmsh.initialize()
+
+# Track down memory leaks
+# from pympler.tracker import SummaryTracker
+#
+# tracker = SummaryTracker()
 
 # Define geometry parameters
 L, H, B = 12e-3, 0.2e-3, 3e-3
@@ -26,13 +42,14 @@ Bmax = B
 grid_size = 0.5e-3
 
 # Select boundary conditions to apply
-bc_z = False
-bc_y = False
-no_eigenvalues = 50
+bc_z = True
+bc_y = True
+no_eigenvalues = 20
+target_frequency = 100e3
 
 # Set a folder to save the features
-folder = "./shape_optimization/shape_optimization/results/scipy1/"
-description = "SHGO optimization, scipy.optimize.shgo(opt_function, bounds)"
+folder = "./me-shape-optimization/results/scipy2/"
+description = "SHGO optimization, scipy.optimize.shgo(opt_function, bounds), bcs on"
 
 os.makedirs(folder, exist_ok=True)
 
@@ -49,29 +66,84 @@ header_lines = [line01, line02, line03, line04]
 with open(folder + "features.csv", "a+") as csvfile:
     csvfile.write("\n".join(header_lines))
 
+
+## Define eigensolver
 eigensolver = SLEPc.EPS().create(MPI.COMM_WORLD)
 
+# Set problem
+eigensolver.setProblemType(SLEPc.EPS.ProblemType.GHEP)
+# Shift and invert mode
+st = eigensolver.getST()
+st.setType(SLEPc.ST.Type.SINVERT)
+# target real eigenvalues
+eigensolver.setWhichEigenpairs(SLEPc.EPS.Which.TARGET_REAL)
+# Set the target frequency
+eigensolver.setTarget(target_frequency**2 * 2 * np.pi)
+# Set no of eigenvalues to compute
+eigensolver.setDimensions(nev=no_eigenvalues)
+
+## Plotting
+# Now define pyvista plotter
+plotter = pyvista.Plotter(off_screen=True)
+
+## Geometry generation
+gmsh.option.setNumber("General.Terminal", 0)
+model = gmsh.model()
+model.add("Box")
+model.setCurrent("Box")
 
 # --------------------
 # Setup function to optimize
 # --------------------
+
+
 def opt_function(horizontal_lengths):
     """
     Function that takes in a list containing the horizontal weights and can be
     optimized with the different scipy optimizers.
     """
+    print("-------------- New Optimization step --------------")
+    # before = psutil.Process(os.getpid()).memory_info().rss / 1024**2
     # Change geometry
-    gmsh_mesh = generate_gmsh_mesh(L, H, B, horizontal_lengths)
+    gmsh_mesh = generate_gmsh_mesh(model, L, H, B, horizontal_lengths)
+
+    # after = psutil.Process(os.getpid()).memory_info().rss / 1024**2
+    # print("Mesh generation: " + str(after - before))
+    # before = after
 
     V, eigenvalues, eigenmodes, first_longitudinal_mode = unified_solving_function(
-        eigensolver, gmsh_mesh, L, H, B, bc_z, bc_y, no_eigenvalues
+        eigensolver,
+        gmsh_mesh,
+        L,
+        H,
+        B,
+        bc_z,
+        bc_y,
+        no_eigenvalues,
+        target_frequency,
     )
-    # Plot first longitudinal mode only
+
+    # after = psutil.Process(os.getpid()).memory_info().rss / 1024**2
+    # print("Solving function: " + str(after - before))
+    # before = after
+
+    # # Plot first longitudinal mode only
     eigenfrequency = np.sqrt(eigenvalues[first_longitudinal_mode].real) / 2 / np.pi
+
     saving_path = folder + "{i:.2f}.png".format(i=eigenfrequency)
     visualise_3D(
-        V, eigenvalues, eigenmodes, first_longitudinal_mode, saving_path, viewup=True
+        plotter,
+        V,
+        eigenvalues,
+        eigenmodes,
+        first_longitudinal_mode,
+        saving_path,
+        viewup=True,
     )
+
+    # after = psutil.Process(os.getpid()).memory_info().rss / 1024**2
+    # print("Visualise 3D function: " + str(after - before))
+    # before = after
 
     # The features should be saved in separate files for each particle!
     append_feature(
@@ -80,10 +152,12 @@ def opt_function(horizontal_lengths):
         horizontal_lengths,
     )
 
+    # after = psutil.Process(os.getpid()).memory_info().rss / 1024**2
+    # print("Append feature function: " + str(after - before))
+    # before = after
+
     return eigenfrequency
 
-
-from scipy.optimize import shgo
 
 bounds = (
     (
@@ -93,4 +167,7 @@ bounds = (
 ) * int(L / grid_size)
 
 
-final_results = shgo(opt_function, bounds)
+# final_results = differential_evolution(opt_function, bounds)
+while True:
+    horizontal_lengths = np.random.uniform(Bmin, Bmax, int(L / grid_size))
+    opt_function(horizontal_lengths)

solver.py
@@ -11,6 +11,7 @@ from slepc4py import SLEPc
 from dolfinx import fem, plot
 import ufl
 from visualisation import visualise_3D, visualise_mesh
+import gc
 
 
 def unified_solving_function(
@@ -111,21 +112,15 @@ def unified_solving_function(
     # Define eigensolver and solve for eigenvalues
     eigensolver.setOperators(K, M)
 
-    eigensolver.setProblemType(SLEPc.EPS.ProblemType.GHEP)
     # tol = 1e-9
     # eigensolver.setTolerances(tol=tol)
-
-    # Shift and invert mode
-    st = eigensolver.getST()
-    st.setType(SLEPc.ST.Type.SINVERT)
-    # target real eigenvalues
-    eigensolver.setWhichEigenpairs(SLEPc.EPS.Which.TARGET_REAL)
-    # Set the target frequency
-    eigensolver.setTarget(target_frequency**2 * 2 * np.pi)
-    # Set no of eigenvalues to compute
-    eigensolver.setDimensions(nev=no_eigenvalues)
     ##
+    # before = psutil.Process(os.getpid()).memory_info().rss / 1024**2
     eigensolver.solve()
+
+    # after = psutil.Process(os.getpid()).memory_info().rss / 1024**2
+    # print("Mesh generation: " + str(after - before))
+    # before = after
 
     """
     Presort for sensible modes
@@ -155,7 +150,9 @@ def unified_solving_function(
     first_longitudinal_eigenmode = determine_first_longitudinal_mode(
         V, eigenmodes, eigenvalues, target_frequency
     )
-    # first_longitudinal_eigenmode = 1
+    """
+    first_longitudinal_eigenmode = 1
+    """
 
     print(
         "Current RAM usage: {0} MB".format(
@@ -286,10 +283,10 @@ def determine_first_longitudinal_mode(V, eigenmodes, eigenvalues, target_frequen
         max_x_warp = maximum.x_warp.to_numpy(int)
         max_x_warp[np.abs(max_x_warp) < 50] = 0
 
-        # min_z_warp = minimum.z_warp.to_numpy(int)
-        # min_z_warp[np.abs(min_z_warp) < 10] = 0
-        # max_z_warp = maximum.z_warp.to_numpy(int)
-        # max_z_warp[np.abs(max_z_warp) < 10] = 0
+        min_z_warp = minimum.z_warp.to_numpy(int)
+        min_z_warp[np.abs(min_z_warp) < 50] = 0
+        max_z_warp = maximum.z_warp.to_numpy(int)
+        max_z_warp[np.abs(max_z_warp) < 50] = 0
 
         df_results.loc[i] = [
             np.all(
@@ -318,18 +315,32 @@ def determine_first_longitudinal_mode(V, eigenmodes, eigenvalues, target_frequen
 
     # Now obtain the first longitudinal mode by selecting for symmetry and
     # minimum y displacement
-    no_of_first_longitudinal_mode = df_results.loc[
-        df_results.y_max
-        == df_results.loc[
-            np.logical_and(
-                df_results.symmetric,
+    try:
+        no_of_first_longitudinal_mode = df_results.loc[
+            df_results.y_max
+            == df_results.loc[
                 np.logical_and(
-                    df_results.eigenfrequency > target_frequency * 0.5,
-                    df_results.eigenfrequency < target_frequency * 1.5,
-                ),
-            )
-        ].y_max.min()
-    ].index[0]
+                    df_results.symmetric,
+                    np.logical_and(
+                        df_results.eigenfrequency > target_frequency * 0.5,
+                        df_results.eigenfrequency < target_frequency * 1.5,
+                    ),
+                )
+            ].y_max.min()
+        ].index[0]
+    except IndexError:
+        no_of_first_longitudinal_mode = df_results.loc[
+            df_results.y_max
+            == df_results.loc[
+                np.logical_and(
+                    df_results.symmetric,
+                    np.logical_and(
+                        df_results.eigenfrequency > target_frequency * 0.5,
+                        df_results.eigenfrequency < target_frequency * 2.0,
+                    ),
+                )
+            ].y_max.min()
+        ].index[0]
 
     print(
         "Mode {0} is the first longitudinal one.".format(no_of_first_longitudinal_mode)
@@ -342,5 +353,4 @@ def determine_first_longitudinal_mode(V, eigenmodes, eigenvalues, target_frequen
             visualise_3D(V, eigenvalues, eigenmodes, mode_no, saving_path)
         print("stop")
     """
-
     return no_of_first_longitudinal_mode