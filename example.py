7ffc0e8f6fb5d2206628f9bde709d304ec81ca90
BuJ1 <JulianButscher@gmx.de>
2023-07-18 13:45:24 +0000

    [example] add gif generation

--- example.py
+++ example.py
@@ -1,11 +1,12 @@
 from geometry_generator import generate_geometry, generate_gmsh_mesh
 from solver import unified_solving_function, determine_first_longitudinal_mode
-from visualisation import visualise_3D, visualise_mesh
+from visualisation import visualise_3D, visualise_mesh, generate_gif
 
 from dolfinx.io.gmshio import model_to_mesh
 
 import sys
 import numpy as np
+import pandas as pd
 import psutil
 import os
 
@@ -34,7 +35,7 @@ bc_z = True
 bc_y = True
 
 no_eigenvalues = 20
-target_frequency = 130e3
+target_frequency = 100e3
 
 ## Define eigensolver
 eigensolver = SLEPc.EPS().create(MPI.COMM_WORLD)
@@ -63,33 +64,33 @@ model.setCurrent("Box")
 
 # Generate random numbers using numpy library with Bmax as a maximum and Bmin as
 # a minimum with L/grid_size entries
-geometry_width_list = np.random.uniform(Bmin, Bmax, int(L / grid_size))
-# geometry_width_list = [
-#     0.003000,
-#     0.003000,
-#     0.003000,
-#     0.003000,
-#     0.003000,
-#     0.003000,
-#     0.003000,
-#     0.003000,
-#     0.003000,
-#     0.003000,
-#     0.003000,
-#     0.003000,
-#     0.003000,
-#     0.003000,
-#     0.003000,
-#     0.003000,
-#     0.003000,
-#     0.001000,
-#     0.003000,
-#     0.001000,
-#     0.003000,
-#     0.001000,
-#     0.003000,
-#     0.003000,
-# ]
+# geometry_width_list = np.random.uniform(Bmin, Bmax, int(L / grid_size))
+geometry_width_list = [
+    0.002322,
+    0.001658,
+    0.002674,
+    0.002289,
+    0.002136,
+    0.002772,
+    0.002401,
+    0.001130,
+    0.002611,
+    0.001769,
+    0.001858,
+    0.002324,
+    0.001219,
+    0.001629,
+    0.001565,
+    0.001819,
+    0.001165,
+    0.001521,
+    0.002848,
+    0.002703,
+    0.002084,
+    0.002877,
+    0.001624,
+    0.001531,
+]
 # geometry_width_list = np.repeat(B, int(L / grid_size))
 
 # With gmsh model
@@ -121,6 +122,25 @@ for mode_no in range(np.size(eigenvalues)):
     visualise_3D(plotter, V, eigenvalues, eigenmodes, mode_no, saving_path)
 
 determine_first_longitudinal_mode(V, eigenmodes, eigenvalues, target_frequency)
+"""
+
+features = pd.read_csv(
+    "./me-shape-optimization/results/dual-annealing-3/features.csv",
+    skiprows=4,
+    names=np.append("frequency", ["width{0}".format(i) for i in range(24)]),
+    sep="\t",
+)
+# Now reduce number of rows and only take every 100th row
+frequencies = np.append(
+    features.frequency.to_numpy()[::500], features.frequency.to_numpy()[-1]
+)
+
+generate_gif(
+    "./me-shape-optimization/results/dual-annealing-3",
+    frequencies,
+)
+"""
+
 
 # Plot first longitudinal mode only
 # freq_3D = np.sqrt(eigenvalues[first_longitudinal_mode].real) / 2 / np.pi
