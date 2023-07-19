481b1809466fad0f42410b78f6879d82f81fa03a
BuJ1 <JulianButscher@gmx.de>
2023-07-19 18:05:18 +0000

    [particle-swarm] Save each particle to separate file

--- particle_swarm.py
+++ particle_swarm.py
@@ -1,6 +1,6 @@
 from geometry_generator import generate_gmsh_mesh
 from solver import unified_solving_function
-from visualisation import visualise_3D, append_feature
+from visualisation import visualise_3D, append_feature, append_text
 
 import sys
 import os
@@ -40,25 +40,6 @@ bc_y = True
 no_eigenvalues = 20
 target_frequency = 100e3
 
-# Set a folder to save the features
-folder = "./me-shape-optimization/results/particle-swarm-4/"
-description = "SHGO optimization, scipy.optimize.shgo(opt_function, bounds), bcs on"
-
-os.makedirs(folder, exist_ok=True)
-
-# Generate a file that contains the meta data in the header
-line01 = "Geometry: L = {0} mm, Bmin = {2} mm, Bmax = {1} mm, H = {2} mm, grid size = {3} mm, accuracy = {4} mm".format(
-    L * 1e3, Bmax * 1e3, Bmin * 1e3, H * 1e3, grid_size * 1e3
-)
-line02 = description
-line03 = "### Simulation Results ###"
-line04 = "Resonance Frequency (Hz)\t Features (m)\n"
-
-header_lines = [line01, line02, line03, line04]
-
-with open(folder + "features.csv", "a+") as csvfile:
-    csvfile.write("\n".join(header_lines))
-
 
 ## Define eigensolver
 eigensolver = slepc4py.SLEPc.EPS().create(MPI.COMM_WORLD)
@@ -89,6 +70,31 @@ model.setCurrent("Box")
 # Setup function to optimize
 # --------------------
 
+# particle swarm functions
+no_particles = 20
+bounds = (Bmin * np.ones(int(L / grid_size)), Bmax * np.ones(int(L / grid_size)))
+min_max_velocities = [-1e-3, 1e-3]
+
+# Set a folder to save the features
+folder = "./me-shape-optimization/results/particle-swarm-7/"
+os.makedirs(folder, exist_ok=True)
+
+# Generate a file that contains the meta data in the header
+line01 = "Geometry: L = {0} mm, Bmin = {2} mm, Bmax = {1} mm, H = {2} mm, grid size = {3} mm, accuracy = {4} mm".format(
+    L * 1e3, Bmax * 1e3, Bmin * 1e3, H * 1e3, grid_size * 1e3
+)
+line02 = "Particle swarm optimization with {0} particles, {1} mm bounds and {2} min and max velocities".format(
+    no_particles, bounds, min_max_velocities
+)
+line03 = "### Simulation Results ###"
+line04 = "Resonance Frequency (Hz)\t Features (m)\n"
+
+header_lines = [line01, line02, line03, line04]
+
+for i in range(no_particles):
+    with open(folder + "features_p{0}.csv".format(i), "a+") as csvfile:
+        csvfile.write("\n".join(header_lines))
+
 
 def opt_function(horizontal_lengths):
     """
@@ -99,7 +105,9 @@ def opt_function(horizontal_lengths):
     n_particles = horizontal_lengths.shape[0]
     # before = psutil.Process(os.getpid()).memory_info().rss / 1024**2
     # Change geometry
+    eigenfrequencies = []
     for i in range(n_particles):
+        print("-- Particle {0} --".format(i))
         gmsh_mesh = generate_gmsh_mesh(model, L, H, B, horizontal_lengths[i])
 
         # after = psutil.Process(os.getpid()).memory_info().rss / 1024**2
@@ -146,45 +154,67 @@ def opt_function(horizontal_lengths):
         # The features should be saved in separate files for each particle!
         append_feature(
             eigenfrequency,
-            folder + "features.csv",
+            folder + "features_p{0}.csv".format(i),
             horizontal_lengths[i],
         )
+        eigenfrequencies.append(eigenfrequency)
 
         # after = psutil.Process(os.getpid()).memory_info().rss / 1024**2
         # print("Append feature function: " + str(after - before))
         # before = after
 
-        return eigenfrequency
+    return eigenfrequencies
 
 
+"""
 def objective(trial):
     # --------------------
     # Setup function to optimize
     # --------------------
     # Set-up hyperparameters
-    c1 = trial.suggest_float("c1", 0, 1.5)
-    c2 = trial.suggest_float("c2", 0, 1.5)
-    w = trial.suggest_float("w", 0, 1.5)
+    c1 = trial.suggest_float("c1", 0, 1)
+    c2 = trial.suggest_float("c2", 0.5, 1.4)
+    w = trial.suggest_float("w", 0.3, 1.0)
     options = {"c1": c1, "c2": c2, "w": w}
-
-    bounds = (Bmin * np.ones(int(L / grid_size)), Bmax * np.ones(int(L / grid_size)))
 
     # Call instance of PSO
     optimizer = ps.single.GlobalBestPSO(
-        n_particles=10,
+        n_particles=no_particles,
         dimensions=int(L / grid_size),
         options=options,
         bounds=bounds,
-        velocity_clamp=[-1e-3, 1e-3],
+        velocity_clamp=min_max_velocities,
     )
 
     # Perform optimization
-    cost, pos = optimizer.optimize(opt_function, iters=1000)
+    cost, pos = optimizer.optimize(opt_function, iters=200)
 
     return cost
 
 
-study = optuna.create_study(direction="minimize")
-study.optimize(objective, n_trials=50)
+study = optuna.create_study(
+    direction="minimize",
+    storage="sqlite:///db.sqlite3",
+    study_name="ps_parameter_search_20p_200iters",
+)
+study.optimize(objective, n_trials=10)
 
 study.best_params
+"""
+
+c1 = 0.5
+c2 = 0.8
+w = 0.5
+options = {"c1": c1, "c2": c2, "w": w}
+
+# Call instance of PSO
+optimizer = ps.single.GlobalBestPSO(
+    n_particles=no_particles,
+    dimensions=int(L / grid_size),
+    options=options,
+    bounds=bounds,
+    velocity_clamp=min_max_velocities,
+)
+
+# Perform optimization
+cost, pos = optimizer.optimize(opt_function, iters=500)
