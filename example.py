6c9c9e048bf56aa476d37c8d609baf4947e43501
BuJ1 <JulianButscher@gmx.de>
2024-10-08 14:55:05 +0000

    Cleaned up example and solver

--- example.py
+++ example.py
@@ -68,104 +68,42 @@ model = gmsh.model()
 model.add("Box")
 model.setCurrent("Box")
 
-# Generate random numbers using numpy library with Bmax as a maximum and Bmin as
-# a minimum with L/grid_size entries
-# geometry_width_list = np.random.uniform(Bmin, Bmax, int(L / grid_size))
-
-# 0.5 mm grid optimization rectangular structure
-# geometry_width_list = [0.003000,0.003000,0.003000,0.003000,0.003000,0.003000,0.001000,0.003000,0.001000,0.001000,0.001000,0.001000,0.001000,0.001000,0.001000,0.001000,0.003000,0.001000,0.003000,0.003000,0.003000,0.003000,0.003000,0.003000]
 # 1 mm grid optimization rectangular structure
-geometry_width_list = [0.005000,0.005000,0.005000,0.0004000,0.0004000,0.0004000,0.0004000,0.0004000,0.0004000,0.005000,0.005000,0.005000]
-
-# 80122 kHz
-# geometry_width_list = [0.001000,0.003000,0.003000,0.001000,0.001000,0.001000,0.001000,0.001000,0.001000,0.001000,0.003000,0.003000]
-# 87580
-# geometry_width_list = [0.001000,0.001000,0.001000,0.003000,0.003000,0.001000,0.001000,0.001000,0.001000,0.001000,0.003000,0.003000]
-# 95014
-# geometry_width_list = [0.001000,0.001000,0.001000,0.003000,0.003000,0.001000,0.001000,0.003000,0.003000,0.001000,0.001000,0.003000]
-# 102506
-# geometry_width_list = [0.001000,0.001000,0.001000,0.003000,0.003000,0.003000,0.003000,0.003000,0.001000,0.001000,0.003000,0.003000]
-# 110016
-# geometry_width_list = [0.001000,0.001000,0.001000,0.003000,0.003000,0.003000,0.001000,0.001000,0.003000,0.001000,0.001000,0.001000]
-# 117464
-# geometry_width_list = [0.003000,0.001000,0.001000,0.003000,0.003000,0.003000,0.003000,0.003000,0.001000,0.001000,0.001000,0.001000]
-# 124968
-# geometry_width_list = [0.001000,0.001000,0.001000,0.003000,0.003000,0.003000,0.003000,0.003000,0.003000,0.001000,0.003000,0.001000]
-# 133896
-# geometry_width_list = [0.001000,0.001000,0.001000,0.001000,0.003000,0.003000,0.003000,0.003000,0.001000,0.001000,0.001000,0.001000]
-# 140682
-# geometry_width_list = [0.001000,0.001000,0.003000,0.003000,0.003000,0.003000,0.003000,0.003000,0.003000,0.003000,0.001000,0.001000]
-# 144163
-# geometry_width_list = [0.001000,0.001000,0.001000,0.001000,0.001000,0.003000,0.003000,0.003000,0.003000,0.003000,0.003000,0.003000,0.003000,0.003000,0.003000,0.003000,0.003000,0.003000,0.003000,0.001000,0.001000,0.001000,0.001000,0.001000]
-
-## Needle
-# 94814
-# geometry_width_list = [0.001000,0.001000,0.001000,0.001000,0.001000,0.001000,0.003000,0.001000,0.003000,0.003000,0.003000,0.003000]
-# 155709
-# geometry_width_list = [0.003000,0.003000,0.003000,0.003000,0.003000,0.003000,0.003000,0.003000,0.001000,0.001000,0.001000,0.001000]
-
-# Standard
-# geometry_width_list = np.repeat(B, int(L / grid_size)/2)
+geometry_width_list = [
+    0.003,
+    0.003,
+    0.003,
+    0.003,
+    0.003,
+    0.003,
+    0.003,
+    0.003,
+    0.003,
+    0.003,
+    0.003,
+    0.003,
+]
 
 # With gmsh model
 # No memory leak
-# geometry_mesh = generate_gmsh_mesh(model, L, H, B, geometry_width_list)
-# geometry_mesh = generate_gmsh_mesh_needle(model, L, H, B, geometry_width_list)
-geometry_mesh = generate_gmsh_mesh_more_crazy_needle(model, L, H, B, np.array([3e-3]))
-# geometry_mesh = generate_gmsh_mesh_different_topologies(model, L, H, B)
-
-"""
-# With fenicsx mesh
-fenics_mesh = generate_geometry(L, H, B)
-"""
-
-# geometry_mesh = gmsh_mesh
-
-# visualise_mesh(geometry_mesh, "mesh.png")
+geometry_mesh = generate_gmsh_mesh(model, L, H, B, geometry_width_list)
 
 # Solve
 V, eigenvalues, eigenmodes, first_longitudinal_mode = unified_solving_function(
     eigensolver, geometry_mesh, L, H, B, bc_z, bc_y, no_eigenvalues
 )
 
-# Show first N eigenmodes
-# Without constraints, the relevant first longitudinal mode is around the 4th mode
-# For only constraining the oscillation in the y-direction, the relevant first longitudinal mode is around the 4th mode
-# For constraining the oscillation in the y & z-direction, the relevant first longitudinal mode is around the 19th mode
-
 # Plot all eigenmodes
 for mode_no in range(np.size(eigenvalues)):
     freq_3D = np.sqrt(eigenvalues[mode_no].real) / 2 / np.pi
     saving_path = "{i:.2f}Hz.png".format(i=freq_3D)
     visualise_3D(
-        plotter, V, eigenvalues, eigenmodes, mode_no, saving_path, viewup=True, high_res=True
+        plotter,
+        V,
+        eigenvalues,
+        eigenmodes,
+        mode_no,
+        saving_path,
+        viewup=True,
+        high_res=True,
     )
-
-# determine_first_longitudinal_mode(V, eigenmodes, eigenvalues, target_frequency)
-"""
-
-features = pd.read_csv(
-    "./me-shape-optimization/results/dual-annealing-3/features.csv",
-    skiprows=4,
-    names=np.append("frequency", ["width{0}".format(i) for i in range(24)]),
-    sep="\t",
-)
-# Now reduce number of rows and only take every 100th row
-frequencies = np.append(
-    features.frequency.to_numpy()[::1000], features.frequency.to_numpy()[-1]
-)
-
-generate_gif(
-    "./me-shape-optimization/results/dual-annealing-3",
-    frequencies,
-)
-
-"""
-
-
-# Plot first longitudinal mode only
-# freq_3D = np.sqrt(eigenvalues[first_longitudinal_mode].real) / 2 / np.pi
-# saving_path = "{i:.2f}.png".format(i=freq_3D)
-# visualise_3D(
-# V, eigenvalues, eigenmodes, first_longitudinal_mode, saving_path, viewup=True
-# )
--- solver.py
+++ solver.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 
 # from scipy.signal import argrelextrema
-from dolfinx import fem, plot,mesh, default_scalar_type
+from dolfinx import fem, plot, mesh, default_scalar_type
 import ufl
 
 from petsc4py import PETSc
@@ -26,45 +26,6 @@ def unified_solving_function(
     no_eigenvalues=25,
     target_frequency=100000,
 ):
-    """
-    # Two materials  
-    Q = fem.FunctionSpace(geometry_mesh, ("DG", 0))
-    metglas_threshold = 0.0 
-
-    # Define subdomains
-    def Omega_0(x):
-        # Metglas
-        return x[1] <= metglas_threshold 
-
-    def Omega_1(x):
-        # PZT
-        return x[1] >= metglas_threshold 
-    
-    mu = fem.Function(Q)
-    lambda_ = fem.Function(Q)
-
-    cells_0 = mesh.locate_entities(geometry_mesh, geometry_mesh.topology.dim, Omega_0)
-    cells_1 = mesh.locate_entities(geometry_mesh, geometry_mesh.topology.dim, Omega_1)
-
-    # PZT
-    E, nu = (5.4e10), (0.34)
-    rho = 7950.0
-    mu_pzt = E / 2.0 / (1 + nu)
-    lambda_pzt = E * nu / (1 + nu) / (1 - 2 * nu)
-
-    # Metglas
-    E, nu = (10e10), (0.3)
-    rho = 7180.0
-    mu_metglas = E / 2.0 / (1 + nu)
-    lambda_metglas = E * nu / (1 + nu) / (1 - 2 * nu)
-
-    mu.x.array[cells_0] = np.full_like(cells_0, mu_pzt, dtype=default_scalar_type)
-    mu.x.array[cells_1] = np.full_like(cells_1, mu_metglas, dtype=default_scalar_type)
-    # lambda_ = lambda_pzt
-
-    lambda_.x.array[cells_0] = np.full_like(cells_0, lambda_pzt, dtype=default_scalar_type)
-    lambda_.x.array[cells_1] = np.full_like(cells_1, lambda_metglas, dtype=default_scalar_type)
-    """
 
     # PZT
     E, nu = (5.4e10), (0.34)
@@ -117,7 +78,6 @@ def unified_solving_function(
 
     # Define actual problem
 
-
     def epsilon(u):
         return 0.5 * (ufl.nabla_grad(u) + ufl.nabla_grad(u).T)
 
@@ -132,11 +92,7 @@ def unified_solving_function(
     k = fem.form(k_form)
     m = fem.form(m_form)
 
-    # One of the big questions is: which boundary conditions do we apply?
-    # I am able to retrieve sensible results for either no boundary conditions, only
-    # boundary condition 1 and both boundary conditions. The resonance frequency
-    # changes quite a bit though.
-
+    # Boundary conditions
     if bc_z == True and bc_y == True:
         bcs_applied = [bc1, bc2]
     if bc_z == True and bc_y == False:
@@ -156,24 +112,11 @@ def unified_solving_function(
     # Define eigensolver and solve for eigenvalues
     eigensolver.setOperators(K, M)
 
-    # tol = 1e-9
-    # eigensolver.setTolerances(tol=tol)
-    ##
-    # before = psutil.Process(os.getpid()).memory_info().rss / 1024**2
     eigensolver.solve()
 
-    # after = psutil.Process(os.getpid()).memory_info().rss / 1024**2
-    # print("Mesh generation: " + str(after - before))
-    # before = after
-
-    """
-    Presort for sensible modes
-    """
+    # Presort for sensible modes
     # Get the number of converged eigenpairs
     evs = eigensolver.getConverged()
-
-    # Create dummy vectors for the eigenvectors to store the results in
-    # vr, vi = K.createVecs()
 
     eigenvalues = []
     eigenmodes = []
@@ -181,14 +124,11 @@ def unified_solving_function(
     for mode_number in range(evs):
         eigenmode = fem.Function(V)
         # Get eigenvalue, eigenvector is saved in vr and vi
-        # eigenvalue = eigensolver.getEigenpair(mode_number, vr, vi)
         eigenvalue = eigensolver.getEigenpair(mode_number, eigenmode.vector)
-        # e_vec = eigensolver.getEigenvector(i, eh.vector)
 
         if ~np.isclose(eigenvalue.real, 1.0, atol=5):
             eigenvalues.append(eigenvalue)
 
-            # eigenmode.vector[:] = vr.getArray()
             eigenmodes.append(eigenmode)
 
     # If this is selected, only the first longitudinal mode is returned
@@ -198,10 +138,6 @@ def unified_solving_function(
         )
     except ValueError:
         raise ValueError("No longitudinal mode found.")
-
-    """
-    first_longitudinal_eigenmode = 1
-    """
 
     print(
         "Current RAM usage: {0} MB".format(
@@ -278,17 +214,7 @@ def determine_first_longitudinal_mode(V, eigenmodes, eigenvalues, target_frequen
                     np.logical_or(np.sign(min_x_warp) == 0, np.sign(max_x_warp) == 0),
                 )
             ),
-            # np.logical_or(
-            # np.sign(min_x_warp) == np.sign(max_x_warp)),
-            # np.sign(min_x_warp) == 0 or np.sign(max_x_warp) == 0,
-            # np.all(np.sign(min_z_warp) == -1 * np.sign(max_z_warp)),
-            # np.allclose(
-            # minimum.x_warp.to_numpy(), maximum.x_warp.to_numpy(), atol=50
-            # ),
-            # np.allclose(
-            # minimum.z_warp.to_numpy(), -maximum.z_warp.to_numpy(), atol=50
-            # ),
-            # ),
+
             eigenfrequency,
             df.x_warp.max(),
             df.y_warp.max(),
@@ -303,49 +229,21 @@ def determine_first_longitudinal_mode(V, eigenmodes, eigenvalues, target_frequen
         no_of_first_longitudinal_mode = df_results.loc[
             np.logical_and(
                 df_results.symmetric == True,
-                np.logical_and(df_results.eigenfrequency <= target_frequency * 1.5, df_results.eigenfrequency >= target_frequency * 0.5),
+                np.logical_and(
+                    df_results.eigenfrequency <= target_frequency * 1.5,
+                    df_results.eigenfrequency >= target_frequency * 0.5,
+                ),
             )
         ].index.to_numpy()[0]
     except:
         raise ValueError("No longitudinal mode found.")
-    """
-    try:
-        no_of_first_longitudinal_mode = df_results.loc[
-            df_results.y_max
-            == df_results.loc[
-                np.logical_and(
-                    df_results.symmetric,
-                    np.logical_and(
-                        df_results.eigenfrequency > target_frequency * 0.5,
-                        df_results.eigenfrequency < target_frequency * 1.5,
-                    ),
-                )
-            ].y_max.min()
-        ].index[0]
-    except IndexError:
-        no_of_first_longitudinal_mode = df_results.loc[
-            df_results.y_max
-            == df_results.loc[
-                np.logical_and(
-                    df_results.symmetric,
-                    np.logical_and(
-                        df_results.eigenfrequency > target_frequency * 0.5,
-                        df_results.eigenfrequency < target_frequency * 2.0,
-                    ),
-                )
-            ].y_max.min()
-        ].index[0]
-    """
+
 
     print(
-        "Mode {0} is the first longitudinal one.".format(no_of_first_longitudinal_mode)
+        "Mode {0} is the first longitudinal mode with a frequency of {1} Hz.".format(
+            no_of_first_longitudinal_mode,
+            df_results.eigenfrequency[no_of_first_longitudinal_mode],
+        )
     )
 
-    """
-    if df.eigenfrequency < 50000 or eigenfrequency > 150000:
-        for mode_no in range(np.size(eigenvalues)):
-            saving_path = "deflection{i}.png".format(i=mode_no)
-            visualise_3D(V, eigenvalues, eigenmodes, mode_no, saving_path)
-        print("stop")
-    """
     return no_of_first_longitudinal_mode
